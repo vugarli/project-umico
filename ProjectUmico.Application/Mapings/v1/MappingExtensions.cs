9964ca67ca0ff7e43745a2af381115fa94242497
Vugar <vuqarss@hotmail.com>
2022-09-23 11:44:46 +0400

    Refactored mapping logic, now custom default values can be enforced easly

---
+++ ProjectUmico.Application/Mapings/v1/MappingExtensions.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+
+namespace ProjectUmico.Application.Mapings;
+
+public static class MappingExtensions
+{
+    public static void ForAllMembersEnforceCustomDefaultValues<TReq,TRes>
+        (this IMappingExpression<TReq,TRes> mappingExpression)
+    {
+        mappingExpression.ForAllMembers(o =>
+        {
+            o.Condition((c, p, requestMember, modelMember, resolutionCtx) =>
+            {
+                // if value is null then do not map. j is request k is model
+                var sourceProp = typeof(TReq)
+                    .GetProperty(o.DestinationMember.Name);
+                var sourceType = sourceProp?.PropertyType as Type;
+                    
+                if (sourceType != null)
+                {
+                    if (requestMember is null || 
+                        (sourceType == typeof(string) && requestMember.ToString() == "string"))
+                    {
+                        return false;
+                    }
+                    else if (sourceType == typeof(int?) && ((int?)requestMember) is null or 0)
+                    {
+                        return false;
+                    }
+                }   
+                return true;
+            });
+        });
+    }
+}
--- ProjectUmico.Application/Mapings/v1/MappingProfile.cs
+++ ProjectUmico.Application/Mapings/v1/MappingProfile.cs
@@ -55,30 +55,6 @@ public class MappingProfile : Profile
         CreateMap<Product, ProductDto>();
 
         CreateMap<UpdateProductCommandV1.UpdateProductCommand, Product>()
-            .ForAllMembers(o =>
-            {
-                o.Condition((c, p, requestMember, modelMember, resolutionCtx) =>
-                {
-                    // if value is null then do not map. j is request k is model
-                    var sourceProp = typeof(UpdateProductCommandV1.UpdateProductCommand)
-                        .GetProperty(o.DestinationMember.Name);
-                    var sourceType = sourceProp?.PropertyType as Type;
-                    
-                    if (sourceType != null)
-                    {
-                        if (requestMember is null || 
-                            (sourceType == typeof(string) && requestMember.ToString() == "string"))
-                        {
-                            return false;
-                        }
-                        else if (sourceType == typeof(int?) && ((int?)requestMember) is null or 0)
-                        {
-                            return false;
-                        }
-                        
-                    }   
-                    return true;
-                });
-            });
+            .ForAllMembersEnforceCustomDefaultValues();
     }
 }
